397c754a0e576213a236cc17569867165c5efa4b
Ansal Jain <jainansal@gmail.com>
2023-08-27 19:09:30 +0530

    update: backend url

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:7000/api/'
+axios.defaults.baseURL = 'https://socials-backend.onrender.com/api/'
 axios.defaults.withCredentials = true
