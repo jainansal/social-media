a86be4479bc11b745f83ebbddef2e2426c9b8bc6
Ansal Jain <jainansal@gmail.com>
2023-08-31 00:28:32 +0530

    update: basic new post style

---
+++ src/components/common/NewPost.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="border-slate-400 border-2 p-2 flex flex-col gap-2 rounded-2xl" @submit.prevent="handleSubmit">
+    <textarea
+      class="w-full h-40 bg-inherit focus:outline-none"
+      placeholder="New post"
+      v-model="newPost"
+      required
+    ></textarea>
+    <button class="bg-white self-end px-2 py-1 text-black rounded-lg">
+      Add
+    </button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const newPost = ref('');
+
+const handleSubmit = () => {
+  console.log(newPost.value)
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,8 +3,8 @@
     <AppNavbar />
     <div class="flex items-start">
       <ProfileCard />
-      <div class="basis-1/2 bg-slate-600 p-4 my-6 mx-3">
-        <LabelSection label="My Posts" />
+      <div class="basis-1/2 my-6 mx-3">
+        <NewPost />
       </div>
       <FriendsCard />
     </div>
@@ -16,6 +16,7 @@ import AppNavbar from "../components/common/AppNavbar.vue";
 import ProfileCard from "../components/profile/ProfileCard.vue";
 import LabelSection from "@/components/common/LabelSection.vue";
 import FriendsCard from "@/components/profile/FriendsCard.vue";
+import NewPost from "../components/common/NewPost.vue";
 </script>
 
 <style lang="scss" scoped>
