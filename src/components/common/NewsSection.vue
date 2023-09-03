3679204fcd15b63aae3bec0e5a884e8d8c865f73
Ansal Jain <jainansal@gmail.com>
2023-09-03 14:33:58 +0530

    upd: news section + api

--- src/components/common/NewsSection.vue
+++ src/components/common/NewsSection.vue
@@ -2,6 +2,16 @@
   <AppLoader :isLoading="isLoading" />
   <div class="basis-1/4 p-4 m-6 ml-3">
     <LabelSection label="News" />
+    <div class="flex flex-col py-4 gap-3">
+      <div
+        v-for="(article, index) in articles"
+        :key="index"
+        class="text-slate-400 hover:text-green-300 cursor-pointer"
+        @click="visit(article.url)"
+      >
+        → {{ article.title }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,18 +23,24 @@ import AppLoader from "@/components/common/AppLoader.vue";
 import newsServices from "@/services/news";
 
 const isLoading = ref(false);
+const articles = ref([]);
 
 const getNews = async () => {
   try {
     isLoading.value = true;
-    await newsServices.getNews(5);
+    const response = await newsServices.getNews();
+    articles.value = response;
   } catch (err) {
     console.log(err);
   } finally {
-    isLoading.value = true;
+    isLoading.value = false;
   }
 };
 getNews();
+
+const visit = (path) => {
+  window.open(path, "_blank");
+};
 </script>
 
 <style lang="scss" scoped>
--- src/services/news.js
+++ src/services/news.js
@@ -1,15 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: 'https://newsapi.org/v2',
-  headers: {
-    'Authorization': 'b44ad62df3f84022a61a49e777c5a628'
-  }
-});
-
-const getNews = async (val) => {
-  const response = await instance.get(`everything?pageSize=${val}`);
-  console.log(response)
+const getNews = async () => {
+  const response = await axios.get('news');
+  return response.data
 }
 
 export default {
