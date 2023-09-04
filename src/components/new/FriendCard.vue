d2116b175e2ec1a64a66a4b4fcb4446524c4e1e8
Ansal Jain <jainansal@gmail.com>
2023-09-04 23:29:45 +0530

    upd: add friends card

---
+++ src/components/new/FriendCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="w-full aspect-square bg-zinc-800 rounded-3xl flex items-center justify-between flex-col p-4"
+  >
+    <img
+      src="https://e1.pxfuel.com/desktop-wallpaper/643/772/desktop-wallpaper-%E2%98%AE%EF%B8%8E-cute-aesthetic-pfp.jpg"
+      alt="pfp"
+      class="w-1/2 aspect-square object-cover rounded-full"
+    />
+    <h1 class="font-semibold text-xl">Gwen Stacy</h1>
+    <div
+      class="w-full p-1 text-center border border-violet-400 rounded-md text-violet-400 cursor-pointer"
+      :class="{
+        'bg-violet-400 text-violet-950': !isFriend,
+      }"
+    >
+      {{ isFriend ? 'Friends' : 'Add as friend'}}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const isFriend = ref(false);
+const toggleIsFriend = () => {
+  isFriend.value = !isFriend.value;
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/new/FriendsView.vue
+++ src/views/new/FriendsView.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="basis-1/2 rounded-3xl">
+  <div class="basis-1/2 rounded-3xl flex flex-col gap-4">
     <FriendsInput />
+    <div class="grid grid-cols-4 gap-4 overlay h-full rounded-3xl">
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+      <FriendCard />
+    </div>
   </div>
 </template>
 
 <script setup>
 import FriendsInput from "../../components/new/FriendsInput.vue";
+import FriendCard from "@/components/new/FriendCard.vue"
 </script>
 
 <style lang="scss" scoped>
