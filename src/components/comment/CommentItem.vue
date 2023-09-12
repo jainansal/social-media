8c8076576ed97d5496a0cce64acda8e8ad7cfa06
Ansal Jain <jainansal@gmail.com>
2023-09-13 00:37:58 +0530

    upd: visit profile from comment

--- src/components/comment/CommentItem.vue
+++ src/components/comment/CommentItem.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="flex gap-2">
-    <h1 class="text-violet-400 cursor-pointer">{{ details.author.name }}</h1>
+    <h1 class="text-violet-400 cursor-pointer" @click="visitProfile">
+      {{ details.author.name }}
+    </h1>
     <p class="font-light">{{ details.content }}</p>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 const props = defineProps({
   details: Object,
 });
+
+const visitProfile = () => {
+  router.push({ path: `/profile/${props.details.author._id}` });
+};
 </script>
 
 <style lang="scss" scoped>
