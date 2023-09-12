071e6af0c91c1df8fb0c49c5962f9d69fbd3d7e1
Ansal Jain <jainansal@gmail.com>
2023-09-13 00:29:52 +0530

    upd: remove dot from requests

--- src/components/nav/NavItem.vue
+++ src/components/nav/NavItem.vue
@@ -8,10 +8,6 @@
   >
     <i class="fa-solid" :class="details.icon"></i>
     {{ details.name }}
-    <div
-      v-if="details.name == 'Requests'"
-      class="w-2 h-2 bg-violet-400 rounded-full"
-    ></div>
   </div>
 </template>
 
